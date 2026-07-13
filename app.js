bd7f3ae0d013e14edde328a7110abf9deb610884
mbiren991-glitch <mbiren991@gmail.com>
2026-07-13 22:53:40 +0530

    Add short generation alert function to app.js

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>ShortsAI - Sign Up</title>
-</head>
-<body>
-    <h1>ShortsAI Sign Up</h1>
-    <p>Coming Soon...</p>
-</body>
-</html>
+function generateShort() {
+    alert("🚀 AI Short generation is coming soon!");
+}
